60cd4785aa6affbe648ee83099409b665a3ef05f
Hui Yu <ityuhui@gmail.com>
2022-11-19 20:16:42 +0800

    19

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ const port = 3000
 
 const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
+const url = "mongodb://192.168.22.121:27017/";
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    var myobj = { name: "Company Inc", address: "Highway 37" };
-    dbo.collection("customers").insertOne(myobj, function(err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-      db.close();
-    });
+app.get('/', async (req, res) => {
+    var html = "";
+    const client = new MongoClient(url);
+    const database = client.db("mydb");
+    const customers = database.collection("customers");
+    const cursor = await customers.find({});
+    await cursor.forEach(doc => html += doc. name);
+
+    res.send(html);
 });
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
